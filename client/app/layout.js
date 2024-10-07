f1b7c57b389b121b5c567f085698d6fef67a6f7c
tarun-hegde <githubtar@gmail.com>
2024-10-08 02:19:29 +0530

    fix : attach favicon

--- client/app/layout.js
+++ client/app/layout.js
@@ -18,7 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
